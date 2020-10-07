264303c134b70e54040b9aa224abce78388f5a39
Michael Satterfield <michael.f.satterfield@gmail.com>
2020-10-06 21:39:23 -0500

    Initial commit

---
+++ src/GuessGame.java
@@ -0,0 +1,11 @@
+public class GuessGame {
+    Player p1;
+    Player p2;
+    Player p3;
+
+    public void startGame() {
+        this.p1 = new Player();
+        this.p2 = new Player();
+        this.p3 = new Player();
+    }
+}
